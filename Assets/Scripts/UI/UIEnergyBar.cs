af8bf16067bc0dc61aa4bc1d1e2149073301f4ac
Andriyanov Denis <andriyanovdenis99@gmail.com>
2021-12-12 02:08:38 +0400

    EnergyBar

---
+++ Assets/Scripts/UI/UIEnergyBar.cs
@@ -0,0 +1,23 @@
+﻿using Gameplay.ShipSystems;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIEnergyBar : MonoBehaviour
+    {
+        [SerializeField] private WeaponSystem _weaponSystem;
+        [SerializeField] private TMP_Text _energy;
+
+        private void Awake()
+        {
+            _weaponSystem.OnEnergyChanged += UpdateEnergyText;
+            UpdateEnergyText();
+        }
+
+        private void UpdateEnergyText()
+        {
+            _energy.text = _weaponSystem.Energy.ToString();
+        }
+    }
+}
